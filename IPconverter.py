434cccd3c1ede9115de59c6081a8a75fef7aae85
byte101 <byte101@users.noreply.github.com>
2017-05-01 19:19:40 -0600

    Add files via upload

---
+++ IPconverter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import socket
+
+
+def IP():
+		
+	website = raw_input("Enter the website: ")
+
+	website_IP = socket.gethostbyname(website)
+
+
+	if website_IP != '198.105.244.28':
+
+		print(website_IP)
+
+	elif website_IP == '198.105.244.28':
+
+		print("website doesn't exist")
+
+IP()
