4bfdb8410822878ce4a8e1d502c6106f61e4e670
metr00 <metr00@users.noreply.github.com>
2017-05-03 22:14:52 -0600

    Add files via upload

--- domain2ip.py
+++ domain2ip.py
@@ -8,18 +8,15 @@ written by metr00
 import socket           													# import the required library
       			                                              
 def IP(domain):         													# define the function IP
-		
-			
+					
 	website_IP = socket.gethostbyname(domain)		
 
-	if website_IP != '198.105.244.28':		
-		
+	if website_IP != '198.105.244.28':				
 		print("========================")
 		print("the IP is " + website_IP)
 		print("========================")
 
-	elif website_IP == '198.105.244.28':
-		
+	elif website_IP == '198.105.244.28':		
 		print("website doesn't exist")
 
 while True:        															      # to loop the IP function         
@@ -29,24 +26,18 @@ while True:        															      # to loop the IP function
 	choice = raw_input("do you want to convert another website? (y/n): ")
 	
 	if choice == 'Y' or choice == 'y':
-		
 		continue
 
-	elif choice == 'N' or choice == 'n':	
-		
+	elif choice == 'N' or choice == 'n':			
 		print("goodbye!")
 		break
 
-	elif choice != 'Y' or choice != 'y' or choice != 'N' or choice != 'n':  # this else-if loop is provided if user provides invalid input
-																		    # keeps looping till correct input
+	elif choice != 'Y' or choice != 'y' or choice != 'N' or choice != 'n':  # this else-if loop is provided if user provides invalid input																		    # keeps looping till correct input
 		while True:
 
 			again = raw_input("invalid input, try again (y/n): ")
-
 			if again == 'Y' or again == 'y':
-
 				break
 
 			elif again == 'N' or again == 'n':
-
 				break
