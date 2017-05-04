de7054d46f1faa0b79a0e1bc6b2ee07704f8af67
metr00 <metr00@users.noreply.github.com>
2017-05-03 22:47:04 -0600

    Add files via upload

--- domain2ip.py
+++ domain2ip.py
@@ -5,11 +5,12 @@ this script is meant to be used with python 2
 written by metr00
 """
 
-import socket           													# import the required library
-      			                                              
-def IP(domain):         													# define the function IP
+import socket           # import the required library
+import sys
+
+def IP(domain):         # define the function IP
 					
-	website_IP = socket.gethostbyname(domain)		
+	website_IP = socket.gethostbyname(domain)
 
 	if website_IP != '198.105.244.28':				
 		print("========================")
@@ -19,20 +20,28 @@ def IP(domain):         													# define the function IP
 	elif website_IP == '198.105.244.28':		
 		print("website doesn't exist")
 
-while True:        															      # to loop the IP function         
+
+
+while True:        	# to loop the IP function         
 	
 	host = raw_input("enter the website you want to convert (ex:'domain.com')\n")	  # asks for user input
-	IP(host)        													          # calls the function IP
-	choice = raw_input("do you want to convert another website? (y/n): ")
+	
+	try:		 		    #while loop if error happens
+		IP(host)        	# calls the function IP
+	except Exception:
+		print("error, try again\n")
+		continue
+
+	choice = raw_input("\ndo you want to convert another website? (y/n): ")
 	
 	if choice == 'Y' or choice == 'y':
 		continue
 
 	elif choice == 'N' or choice == 'n':			
 		print("goodbye!")
-		break
+		sys.exit()
 
-	elif choice != 'Y' or choice != 'y' or choice != 'N' or choice != 'n':  # this else-if loop is provided if user provides invalid input																		    # keeps looping till correct input
+	elif choice != 'Y' or choice != 'y' or choice != 'N' or choice != 'n':  # this else-if loop is provided if user provides invalid input			
 		while True:
 
 			again = raw_input("invalid input, try again (y/n): ")
@@ -40,4 +49,4 @@ while True:        															      # to loop the IP function
 				break
 
 			elif again == 'N' or again == 'n':
-				break
+				sys.exit()
