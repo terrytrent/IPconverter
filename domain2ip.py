5a4ed74e92719a91e8a1b5f502461714620cbbab
Terry Trent <terrytrent@users.noreply.github.com>
2017-05-17 10:37:40 -0400

    Update domain2ip.py
    Modified to work properly from command line without looping and using the main functionality

--- domain2ip.py
+++ domain2ip.py
@@ -1,35 +1,28 @@
-#!/usr/bin/env python
-"""
-A simple script that converts domains into IP addresses
-this script is meant to be used with python 2
-written by metr00
-"""
-
-import socket           # import the required library
+import socket
 import sys
 
-def IP(domain):         # define the function IP
-					
-	website_IP = socket.gethostbyname(domain)
 
-	if website_IP != '198.105.244.28':				
-		print("========================")
-		print("the IP is " + website_IP)
-		print("========================")
+def get_ip(domain):
 
-	elif website_IP == '198.105.244.28':		
-		print("website doesn't exist")
+    try:
+        website_IP = socket.gethostbyname(domain)
+        return website_IP
+    except:
+        return False
 
+if __name__ == "__main__":
+    arguments = sys.argv  # uses the commandline argument
 
+    if len(arguments) != 2:
+        print("usage: ./domain2ip example.com")
+    else:
+        host = arguments[1]
 
-while True:        	# to loop the IP function         
-	
-	host = sys.argv	  # uses the commandline argument
-	
-	try:		 		    # while loop if error happens
-		IP(host)        	# calls the function IP
-	except Exception:
-		print("usage: ./domain2ip example.com")
-		continue
+        host_ip = get_ip(host)
 
-	
+        if not host_ip:
+            print("The website '{0}' does not exist or cannot be found".format(host))
+        else:
+            print("========================")
+            print("the IP is " + host_ip)
+            print("========================")
